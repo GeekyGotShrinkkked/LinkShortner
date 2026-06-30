fc523cd5eff68873dc2cddf42407855559ca1147
Arpit Awasthi <aparnaarpitawasthi@gmail.com>
2026-06-30 11:39:43 +0530

    feat: Define Post route

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import dotenv from "dotenv"; 
 import express from "express";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
+import urlRoutes from "./routes/url.js";
 
+dotenv.config();
 
 connectDB();
 
 
 const app = express();
+app.use(express.json());
+
+//Home route
 app.get('/',(req,res) => {
     res.send('API is running with nodemon');
 });
 
+app.use("/api",urlRoutes);
 
 const PORT = process.env.PORT || 5000;
 
